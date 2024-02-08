e514ee374835896995cc5f15a7f1fbbcdf3f6ede
fvaldes0109 <fvaldes0109@gmail.com>
2024-02-08 17:54:12 +0100

    First commit

---
+++ src/main/java/com/example/harbourquests/HarbourquestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.harbourquests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarbourquestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarbourquestsApplication.class, args);
+	}
+
+}
