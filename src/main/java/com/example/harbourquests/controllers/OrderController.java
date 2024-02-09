7816433f0ff7a6de24fcfc8d82427f699c443af3
fvaldes0109 <fvaldes0109@gmail.com>
2024-02-09 10:26:33 +0100

    Add orders PUT and index GET without filters

--- src/main/java/com/example/harbourquests/controllers/OrderController.java
+++ src/main/java/com/example/harbourquests/controllers/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.harbourquests.data.entities.Order;
 import com.example.harbourquests.data.repositories.OrderRepository;
+import com.example.harbourquests.enums.OrderStatus;
 
 @RestController
 public class OrderController {
@@ -13,12 +14,23 @@ public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
 
+    public Iterable<Order> getOrders(OrderStatus status) {
+        
+        return orderRepository.findAll();
+    }
+
     public @ResponseBody Order getOrderById(Long orderId) {
         return orderRepository.findById(orderId).get();
     }
 
     public Order createOrder(Order entity) {
-
         return orderRepository.save(entity);
+    }
+
+    public Order updateOrderStatus(Long id, OrderStatus status) {
+        
+        Order order = orderRepository.findById(id).get();
+        order.setStatus(status);
+        return orderRepository.save(order);
     }
 }
--- src/main/java/com/example/harbourquests/routes/ApiRouter.java
+++ src/main/java/com/example/harbourquests/routes/ApiRouter.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.harbourquests.controllers.OrderController;
 import com.example.harbourquests.data.entities.Order;
+import com.example.harbourquests.enums.OrderStatus;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 
@@ -23,6 +28,12 @@ public class ApiRouter {
         this.orderController = orderController;
     }
     
+    @GetMapping("order")
+    public Iterable<Order> getOrders(@RequestParam(required = false) OrderStatus status) {
+        return orderController.getOrders(status);
+    }
+    
+
     @GetMapping("order/{orderId}")
     public Order getOrderById(@PathVariable Long orderId) {
         return orderController.getOrderById(orderId);   
@@ -30,8 +41,11 @@ public class ApiRouter {
     
     @PostMapping("order")
     public Order createOrder(@RequestBody Order entity) {
-        
         return orderController.createOrder(entity);
     }
-    
+
+    @PutMapping("order/{id}")
+    public Order updateOrderStatus(@PathVariable Long id, @RequestBody Order entity) {
+        return orderController.updateOrderStatus(id, entity.getStatus());
+    }
 }
