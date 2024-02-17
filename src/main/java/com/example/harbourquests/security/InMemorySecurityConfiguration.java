06761d0a5cd590462f6ec596d87a8ac26d7ee9ac
fvaldes0109 <fvaldes0109@gmail.com>
2024-02-18 00:05:34 +0100

    Add authentication to all routes

---
+++ src/main/java/com/example/harbourquests/security/InMemorySecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.harbourquests.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class InMemorySecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService =
+                new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("john")
+                .password("12345")
+                .roles("COURIER", "ADMIN")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
